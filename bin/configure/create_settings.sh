08b687ca79ce61e2503eecb2d102b2d213e96601
Miguel Silva <miguasjpsilva@gmail.com>
2019-10-14 02:36:54 +0100

    Update create_settings.sh
    Changed:
     - Default values and descriptiosn for user ease

--- bin/configure/create_settings.sh
+++ bin/configure/create_settings.sh
@@ -13,17 +13,27 @@ if [ -f $settingsDir ]; then
 	fi
 fi
 
-read -p 'Environment folder name: ' environment
+env_dialog="Environment folder name(leave empty for detected $(uname -o)): "
+read -p "$env_dialog" -i "$uname -o" environment
+if [ -z $environment ]; then
+	environment=$(uname -o | tr '[:upper:]' '[:lower:]')
+fi
+
 read -p 'Locale folder name: ' locale
 read -p 'Bind IP: ' bind_ip
 read -p 'SQL Database IP: ' db_ip
-read -p 'SQL Database Port: ' db_port
+read -p 'SQL Database Port(leave empty for default 29999): ' db_port
+if [ -z $db_port ]; then
+	db_port="29999"
+fi
+
 read -p 'SQL Database User: ' db_user
 
 echo -ne "Would you like to generate a random password for SQL Database Password? (y/n): "
 read answer
 if [ "$answer" != "${answer#[Yy]}" ]; then
 	db_password=$(openssl rand -base64 16)
+	db_password_generated=1
 	#echo "Generated password: $db_password"
 else
 	echo -ne "Type the password(input is hidden): "
@@ -31,14 +41,23 @@ else
 	echo -e ""
 fi
 
-read -p 'Base Game Port(where core ports are based of): ' base_game_port
-read -p 'Base Game 99 Port(where core ports are based of): ' base_game_99_port
+read -p 'Base Game Port(where core ports are based of, default 20000): ' base_game_port
+if [ -z $base_game_port ]; then
+	base_game_port="20000"
+fi
+
+read -p 'Base Game 99 Port(where core ports are based of, default 20000): ' base_game_99_port
+if [ -z $base_game_99_port ]; then
+	base_game_99_port="20000"
+fi
+
 read -p 'Adminpage IP: ' adminpage_ip
 
 echo -ne "Would you like to generate a random password for Adminpage Password? (y/n): "
 read answer
 if [ "$answer" != "${answer#[Yy]}" ]; then
 	adminpage_password=$(openssl rand -base64 16)
+	adminpage_password_generate=1
 	#echo "Generated password: $adminpage_password"
 else
 	echo -ne "Type the password(input is hidden): "
@@ -47,7 +66,10 @@ else
 fi
 
 read -p 'Mall URL: ' mall_url
-read -p 'Backup Databases: ' backup_databases
+read -p "Backup Databases(default is account common log player): " backup_databases
+if [ -z $backup_databases ]; then
+	backup_databases="account common log player"
+fi
 
 echo ""
 echo -e "\e[32mAre you sure of these settings:\e[0m"
@@ -57,11 +79,13 @@ echo "Bind IP: $bind_ip"
 echo "SQL Database IP: $db_ip"
 echo "SQL Database Port: $db_port"
 echo "SQL Database User: $db_user"
-echo "SQL Database Password: (hidden)"
+echo -ne "SQL Database Password: "
+[[ $db_password_generated -eq 1 ]]; echo "$db_password" || echo "(hidden)"
 echo "Base Game Port: $base_game_port"
 echo "Base Game 99 Port: $base_game_99_port"
 echo "Adminpage IP: $adminpage_ip"
-echo "Adminpage Password: (hidden)"
+echo -ne "Adminpage Password: "
+[[ $adminpage_password_generate -eq 1 ]]; echo "$adminpage_password" || echo "(hidden)"
 echo "Mall URL: $mall_url"
 echo "Backup Databases: $backup_databases"
 echo ""
